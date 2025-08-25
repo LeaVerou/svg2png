21491a8e83346a72ded71e350b234a2cee519cc5
Lea Verou <lea@verou.me>
2025-08-25 19:29:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,109 @@
+let url;
+let width, height;
+let naturalWidth, naturalHeight, aspectRatio;
+
+let canvas = document.getElementById('canvas');
+let png;
+
+file_input.onchange = evt => {
+	let file = evt.target.files[0];
+	download_link.download = file.name.replace('.svg', '.png');
+
+	if (url) {
+		URL.revokeObjectURL(url);
+	}
+
+	url = URL.createObjectURL(file);
+	img.src = url;
+}
+
+img.onload = async () => {
+	await img.decode();
+
+	naturalWidth = img.naturalWidth;
+	naturalHeight = img.naturalHeight;
+	aspectRatio = naturalWidth / naturalHeight;
+
+	w.placeholder ||= naturalWidth;
+	h.placeholder ||= naturalHeight;
+
+	w.oninput();
+}
+
+w.oninput = h.oninput = padding_input.oninput = evt => {
+	if (w.value) {
+		h.placeholder = Math.round(w.valueAsNumber / aspectRatio);
+	}
+	else {
+		h.placeholder = naturalHeight;
+	}
+
+	if (h.value) {
+		w.placeholder = Math.round(h.valueAsNumber * aspectRatio);
+	}
+	else {
+		w.placeholder = naturalWidth;
+	}
+
+	let forceWidth = w.value;
+	let forceHeight = h.value;
+
+	if (forceWidth || forceHeight) {
+		if (forceWidth) {
+			forceWidth = Number(forceWidth);
+			img.style.width = forceWidth + 'px';
+		}
+		if (forceHeight) {
+			forceHeight = Number(forceHeight);
+			img.style.height = forceHeight + 'px';
+		}
+	}
+
+	width = undefined;
+	height = undefined;
+
+	if (forceWidth) {
+		width = forceWidth;
+
+		if (forceHeight) {
+			height = width / aspectRatio;
+		}
+	}
+	else if (forceHeight) {
+		height = forceHeight;
+		width = height * aspectRatio;
+	}
+	else {
+		width = naturalWidth;
+		height = naturalHeight;
+	}
+
+	paint();
+}
+
+function paint() {
+	canvas.width = width;
+	canvas.height = height;
+	canvas.style.zoom = 1/devicePixelRatio;
+	png_img.style.zoom = 1/devicePixelRatio;
+
+	let padding = Number(padding_input.value) || 0;
+
+	let ctx = canvas.getContext('2d', {
+		colorSpace: "display-p3",
+		colorType: "float16",
+	});
+	ctx.drawImage(img, padding, padding, width - padding * 2, height - padding * 2);
+
+	if (png) {
+		URL.revokeObjectURL(png);
+	}
+
+	canvas.toBlob(blob => {
+		if (blob) {
+			png = URL.createObjectURL(blob);
+			png_img.src = png;
+			download_link.href = png;
+		}
+	}, 'image/png');
+}
